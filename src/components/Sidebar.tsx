f2c7372e197dcf1f81a41001d7b2d1420560a544
Gilbert-2 <gilbertrick81@gmail.com>
2025-06-23 13:44:32 +0200

    reverted sidebar

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,12 +1,10 @@
 import { Outlet } from "react-router-dom";
-import { Sidebar as ResponsiveSidebar, SidebarProvider } from "./ui/sidebar";
+import { Sidebar as AppSidebar } from "./ui/sidebar";
 
 export function Sidebar() {
   return (
     <div className="flex flex-1 h-screen overflow-hidden">
-      <SidebarProvider>
-        <ResponsiveSidebar />
-      </SidebarProvider>
+      <AppSidebar />
       <main className="flex-1 overflow-auto">
         <Outlet />
       </main>
